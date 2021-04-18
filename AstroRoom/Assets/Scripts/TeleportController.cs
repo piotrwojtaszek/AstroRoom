45b522c0d0c80f7a12af42d0d3b424bdd0706361
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-04-18 13:21:02 +0200

    Base shift

---
+++ AstroRoom/Assets/Scripts/TeleportController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using System;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject baseControllerGameoObject;
+    public GameObject teleportationGameObject;
+
+    public InputActionReference teleportationActivationReference;
+
+    [Space]
+    public UnityEvent onTeleportationActive;
+    public UnityEvent onTeleportationCancel;
+
+    private void Start()
+    {
+        teleportationActivationReference.action.performed += TeleportationActive;
+        teleportationActivationReference.action.canceled += TeleportationCancel;
+    }
+
+    private void TeleportationCancel(InputAction.CallbackContext obj) => Invoke("DeactivateTeleporter", .1f);
+
+    private void DeactivateTeleporter() => onTeleportationCancel.Invoke();
+
+    private void TeleportationActive(InputAction.CallbackContext obj) => onTeleportationActive.Invoke();
+}
