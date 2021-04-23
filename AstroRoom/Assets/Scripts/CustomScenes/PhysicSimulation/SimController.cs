49d45447b497cf511e09612622c5323f165ded7f
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-04-23 15:39:30 +0200

    Base2

---
+++ AstroRoom/Assets/Scripts/CustomScenes/PhysicSimulation/SimController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimController : MonoBehaviour
+{
+    SimGravityObject[] bodies;
+    private static SimController _instance;
+    public static SimController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<SimController>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        bodies = FindObjectsOfType<SimGravityObject>();
+    }
+
+    private void FixedUpdate()
+    {
+        for(int i = 0; i < bodies.Length; i++)
+        {
+            bodies[i].Move(bodies);
+        }
+    }
+
+    private void SimulatedFixedUpdate()
+    {
+        for (int i = 0; i < bodies.Length; i++)
+        {
+            bodies[i].Move(bodies);
+        }
+    }
+
+    public float timeStep = .01f;
+    public float gravitionalConstant = 66.7f;
+}
---
+++ AstroRoom/Assets/Scripts/CustomScenes/PhysicSimulation/SimGravityObject.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimGravityObject : MonoBehaviour
+{
+    public BodyType bodyType = BodyType.Planet;
+    //jesli moon to orbituje dookola najblizszej planety
+    //jesli planeta to orbituje dookola slonca
+    //jesli static to static
+    //jesli free to oddzialuje wszystko 
+
+    public Rigidbody rb { get; set; }
+    public SimGravityObject relativeTo;
+
+    public Vector3 initialVelocity;
+
+    [HideInInspector]
+    public Vector3 velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        velocity = initialVelocity;
+    }
+
+    private void UpdateVelocity(SimGravityObject[] allBodies)
+    {
+        foreach (var otherBody in allBodies)
+        {
+            if (otherBody != this)
+            {
+                float sqrDst = (otherBody.rb.position - rb.position).sqrMagnitude;
+                Vector3 forceDir = (otherBody.rb.position - rb.position).normalized;
+
+                Vector3 acceleration = SimController.Instance.gravitionalConstant * forceDir * otherBody.rb.mass / sqrDst;
+                velocity += acceleration * SimController.Instance.timeStep;
+            }
+
+        }
+    }
+
+    private void UpdateVelocity(SimGravityObject otherBody)
+    {
+
+        if (relativeTo == null)
+            return;
+
+        float sqrDst = (otherBody.rb.position - rb.position).sqrMagnitude;
+        Vector3 forceDir = (otherBody.rb.position - rb.position).normalized;
+
+        Vector3 acceleration = SimController.Instance.gravitionalConstant * forceDir  * otherBody.rb.mass / sqrDst;
+        velocity += acceleration * SimController.Instance.timeStep;
+    }
+
+    public void UpdateVelocity(float timeStep)
+    {
+        rb.MovePosition(rb.position + velocity * timeStep);
+    }
+
+    public void Move(SimGravityObject[] allBodies)
+    {
+        if (bodyType == BodyType.Static)
+            return;
+
+        if (bodyType == BodyType.Free)
+            UpdateVelocity(allBodies);
+
+        if (bodyType == BodyType.Moon)
+            UpdateVelocity(relativeTo);
+        UpdateVelocity(SimController.Instance.timeStep);
+    }
+}
+
+public enum BodyType
+{
+    Static,
+    Planet,
+    Moon,
+    Free
+}
