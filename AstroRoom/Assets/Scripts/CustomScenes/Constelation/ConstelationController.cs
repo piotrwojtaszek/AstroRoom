b72ff57ed604937a847fe1953c6114d0d1940aa3
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-05-06 17:15:35 +0200

    Done

--- AstroRoom/Assets/Scripts/CustomScenes/Constelation/ConstelationController.cs
+++ AstroRoom/Assets/Scripts/CustomScenes/Constelation/ConstelationController.cs
@@ -5,22 +5,26 @@ using System;
 public class ConstelationController : MonoBehaviour
 {
     public static ConstelationController instance;
-    public SOConstelationBase constelationPreset;
+    public SOConstelationBase ConstelationPreset { get { return constelationPreset; } }
+    [SerializeField]
+    private SOConstelationBase constelationPreset;
     public GameObject socket;
     public GameObject edge;
     public bool[,] adjMatrix;
+    public bool[,] adjMatrixCheck;
     public Transform[] nodes;
     public List<int> selected = new List<int>();
     private void Awake()
     {
         instance = this;
         string temp = "";
-        adjMatrix = new bool[constelationPreset.size, constelationPreset.size];
-        for (int x = 0; x < constelationPreset.size; x++)
+        adjMatrix = new bool[ConstelationPreset.size, ConstelationPreset.size];
+        adjMatrixCheck = new bool[ConstelationPreset.size, ConstelationPreset.size];
+        for (int x = 0; x < ConstelationPreset.size; x++)
         {
-            for (int y = 0; y < constelationPreset.size; y++)
+            for (int y = 0; y < ConstelationPreset.size; y++)
             {
-                adjMatrix[x, y] = constelationPreset.adjMatrix[x * constelationPreset.size + y];
+                adjMatrix[x, y] = ConstelationPreset.adjMatrix[x * ConstelationPreset.size + y];
                 if (adjMatrix[x, y])
                 {
                     temp += "1 ";
@@ -37,9 +41,9 @@ public class ConstelationController : MonoBehaviour
             temp += "\n";
         }
         Debug.Log(temp);
-
+        nodes = new Transform[ConstelationPreset.size];
         int i = 0;
-        foreach (Node node in constelationPreset.nodes)
+        foreach (Node node in ConstelationPreset.nodes)
         {
             GameObject nodeObj = Instantiate(socket, transform);
             nodeObj.transform.localPosition = node.position;
@@ -52,15 +56,17 @@ public class ConstelationController : MonoBehaviour
     public void CheckConnection()
     {
         if (selected.Count == 2)
-            if (adjMatrix[selected[0], selected[1]] == true)
+        {
+            if (adjMatrix[selected[0], selected[1]] == true && adjMatrixCheck[selected[0], selected[1]] == false)
             {
+                adjMatrixCheck[selected[0], selected[1]] = true;
+                adjMatrixCheck[selected[1], selected[0]] = true;
                 GameObject lineObj = Instantiate(edge, transform);
                 ConstelationEdge lineControler = lineObj.GetComponent<ConstelationEdge>();
                 lineControler.id_1 = selected[0];
                 lineControler.id_2 = selected[1];
-                selected.Clear();
             }
+            selected.Clear();
+        }
     }
-
-
 }
