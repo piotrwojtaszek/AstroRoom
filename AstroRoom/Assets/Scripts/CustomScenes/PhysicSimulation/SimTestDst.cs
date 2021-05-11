de3cc7dee5ed92711794ccc856f841fdb05a80e7
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-05-11 18:33:53 +0200

    Good enough

--- AstroRoom/Assets/Scripts/CustomScenes/PhysicSimulation/SimTestDst.cs
+++ AstroRoom/Assets/Scripts/CustomScenes/PhysicSimulation/SimTestDst.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteInEditMode]
 public class SimTestDst : MonoBehaviour
 {
     private SimGravityObject relativeTo;
     public SimGravityObject gravityObject;
-
+    public float orbitalTime;
     void Start()
     {
-        relativeTo = GetComponent<SimGravityObject>().relativeTo;
-        gravityObject = GetComponent<SimGravityObject>();
+        /*        relativeTo = GetComponent<SimGravityObject>().relativeTo;
+                gravityObject = GetComponent<SimGravityObject>();*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log((relativeTo.transform.position - gravityObject.transform.position).magnitude);
+        if (gravityObject == null)
+            gravityObject = GetComponent<SimGravityObject>();
+
+
+        float dst = (Vector3.zero - gravityObject.transform.position).magnitude;
+        float okres = (2f * Mathf.PI * dst / gravityObject.initialVelocity.magnitude);
+        orbitalTime = okres * Time.deltaTime/SimController.Instance.timeStep;
+
     }
 }
