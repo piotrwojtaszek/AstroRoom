705aee69cf5179b7c92be4c260eddb9d505f7fc8
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-05-14 16:59:51 +0200

    Zoom

--- AstroRoom/Assets/Scripts/CustomScenes/SolarEclipse/SolarEclipseController.cs
+++ AstroRoom/Assets/Scripts/CustomScenes/SolarEclipse/SolarEclipseController.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
-
+using UnityEngine.UI;
+using TMPro;
 public class SolarEclipseController : MonoBehaviour
 {
     public Transform moonPivot;
@@ -11,6 +12,12 @@ public class SolarEclipseController : MonoBehaviour
     List<InputDevice> inputDevices = new List<InputDevice>();
     float xAngle = 0f;
     float yAngle = 0f;
+    float distance = 200f;
+    [SerializeField]
+    [Range(0.75f, 1.75f)]
+    float zoom = 1f;
+    public Slider slider;
+    public TextMeshProUGUI uGUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +29,15 @@ public class SolarEclipseController : MonoBehaviour
     {
         foreach (var device in inputDevices)
         {
-            /* Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.characteristics));*/
             device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 inputValue);
-            yAngle = Mathf.Lerp(yAngle, inputValue.y, 0.01f);
-            xAngle = Mathf.Lerp(xAngle, inputValue.x, 0.01f);
-            moonPivot.RotateAround(Vector3.zero, Vector3.up, xAngle * Time.deltaTime * 10f);
-            moonPivot.RotateAround(Vector3.zero, Vector3.left, yAngle * Time.deltaTime * 10f);
-
-            /*  Debug.Log(inputValue);*/
+            yAngle = Mathf.Lerp(yAngle, inputValue.y, Time.deltaTime * 0.4f);
+            xAngle = Mathf.Lerp(xAngle, inputValue.x, Time.deltaTime * 0.4f);
+            moonPivot.RotateAround(Vector3.zero, Vector3.up, xAngle * Time.deltaTime * 5f);
+            moonPivot.RotateAround(Vector3.zero, Vector3.left, yAngle * Time.deltaTime * 5f);
         }
+        ProcessZoom();
+        zoom = slider.value;
+        uGUI.text = "ZOOM: " + slider.value.ToString("F1") + "x";
     }
 
     void UpdateDevices()
@@ -43,5 +50,12 @@ public class SolarEclipseController : MonoBehaviour
         Vector3 dir = (sun.position - Vector3.zero).normalized;
         float dst = (moonPivot.position - Vector3.zero).magnitude;
         Gizmos.DrawSphere(dir * dst, 10f);
+    }
+
+    void ProcessZoom()
+    {
+
+        Vector3 newPosition = (moonPivot.position - Vector3.zero).normalized * distance * (1f / zoom);
+        moonPivot.position = Vector3.Lerp(moonPivot.position, newPosition, Time.deltaTime * 0.35f);
     }
 }
