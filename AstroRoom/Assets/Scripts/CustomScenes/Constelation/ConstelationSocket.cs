3850249ee298559b4013c8831de95c8aeb9825c1
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-05-21 13:19:32 +0200

    Done

--- AstroRoom/Assets/Scripts/CustomScenes/Constelation/ConstelationSocket.cs
+++ AstroRoom/Assets/Scripts/CustomScenes/Constelation/ConstelationSocket.cs
@@ -7,18 +7,26 @@ using UnityEngine.Events;
 public class ConstelationSocket : INode
 {
     Color baseColor;
-    public List<InputActionReference> socketActivationReference;
     public ConstelationController constelationController;
 
     [Space]
     public UnityEvent<int> onSocketHoverEnter;
     public UnityEvent<int> onSocketHoverExit;
+
+    public UnityAction onSelect;
+    public UnityAction onDeselect;
     // Start is called before the first frame update
     void Start()
     {//a hdyby dodawaæ na hover Action do triggera , a na exit usuwaæ ?????
         baseColor = GetComponent<Renderer>().material.color;
         constelationController.onReady += EnableCollider;
         constelationController.onComplete += DisableCollider;
+        constelationController.onComplete += ClearSelected;
+        constelationController.onComplete += SetBaseColor;
+
+
+        onSelect += SetSelectedColor;
+        onDeselect += SetBaseColor;
     }
     private void Update()
     {
@@ -26,12 +34,12 @@ public class ConstelationSocket : INode
         //zamiast tego zrobiæ Action w ConstelationCOntroller która jest wywo³ywana w momencie zaznaczenia
         if (constelationController.selected.Contains(id))
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            onSelect?.Invoke();
 
         }
         else
         {
-            GetComponent<Renderer>().material.color = baseColor;
+            onDeselect?.Invoke();
         }
     }
 
@@ -60,5 +68,20 @@ public class ConstelationSocket : INode
     public void DisableCollider()
     {
         GetComponent<Collider>().enabled = false;
+    }
+
+    public void SetBaseColor()
+    {
+        GetComponent<Renderer>().material.color = baseColor;
+    }
+
+    public void SetSelectedColor()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void ClearSelected()
+    {
+        constelationController.selected.Clear();
     }
 }
