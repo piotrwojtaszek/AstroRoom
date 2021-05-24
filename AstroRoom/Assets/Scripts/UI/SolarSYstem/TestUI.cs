c2a8b342d28c34ae9f4b800c46c32cd784e5f74e
Piotr Wojtaszek <48589767+piotrwojtaszek@users.noreply.github.com>
2021-05-25 01:27:01 +0200

    Done

--- AstroRoom/Assets/Scripts/UI/SolarSYstem/TestUI.cs
+++ AstroRoom/Assets/Scripts/UI/SolarSYstem/TestUI.cs
@@ -5,21 +5,7 @@ using TMPro;
 using UnityEngine.UI;
 public class TestUI : MonoBehaviour
 {
-    public TextMeshProUGUI textTimeStep;
     public Slider slider;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        slider.value = SimController.Instance.timeStep;
-        textTimeStep.text = SimController.Instance.timeStep.ToString("F3");
-    }
     public void Change()
     {
         SimController.Instance.timeStep = slider.value;
